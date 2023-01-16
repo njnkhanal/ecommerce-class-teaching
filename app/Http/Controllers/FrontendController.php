14a0807067a9c06e64cfdf680a80a9aa4244f101
Njn khanal <45514077+njnkhanal@users.noreply.github.com>
2023-01-16 21:14:58 +0545

    making carousel dynamic

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\banner;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
     public function homepage()
     {
-        return view('frontend.pages.homepage');
+        $banners = banner::all();
+        return view('frontend.pages.homepage', compact('banners'));
     }
 }
--- resources/views/frontend/pages/homepage.blade.php
+++ resources/views/frontend/pages/homepage.blade.php
@@ -4,23 +4,17 @@
     <section class="carousel-main">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                @foreach ($banners as $key => $banner)
+                    <li data-target="#carouselExampleIndicators" data-slide-to="{{ $key }}"
+                        class="{{ $key == 0 ? 'active' : '' }}"></li>
+                @endforeach
             </ol>
             <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src="http://aaviss.com.np/uploads/contents//image2108050444237saygo.png"
-                        alt="First slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="http://aaviss.com.np/uploads/contents//image2108050444237saygo.png"
-                        alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="http://aaviss.com.np/uploads/contents//image2108050444237saygo.png"
-                        alt="Third slide">
-                </div>
+                @foreach ($banners as $key => $banner)
+                    <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
+                        <img class="d-block w-100" src="{{ asset($banner->image) }}" alt="First slide">
+                    </div>
+                @endforeach
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
